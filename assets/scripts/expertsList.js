51c46fa80c5d4bd2669851d03842d6a63c0155e0
Djibril-Mugisho <mugishodjibril2004@gmail>
2023-05-24 01:51:06 +0300

    Fix linters errors

--- assets/scripts/expertsList.js
+++ assets/scripts/expertsList.js
@@ -1,48 +1,47 @@
-const mobileTeamListContainer = document.querySelectorAll(".mobile-team-list");
+const mobileTeamListContainer = document.querySelectorAll('.mobile-team-list');
 
 const teamMembers = [
-    {
-        name: "mark orlando",
-        role: "CEO",
-        bio: 'Ex manager ate chanel, Mark Lee is an incredible fashion enthusiast and fashion professor',
-        image: "assets/images/teamm1.png"
-    },
-    {
+  {
+    name: 'mark orlando',
+    role: 'CEO',
+    bio: 'Ex manager ate chanel, Mark Lee is an incredible fashion enthusiast and fashion professor',
+    image: 'assets/images/teamm1.png',
+  },
+  {
 
-        name: "Juliet Denis",
-        role: "Chief of Designer",
-        bio: 'Professional designer and chief executive , Juliet Denis has an incredible experience in the field of fashion design',
-        image: "assets/images/teamm2.png"
-    },
-    {
+    name: 'Juliet Denis',
+    role: 'Chief of Designer',
+    bio: 'Professional designer and chief executive , Juliet Denis has an incredible experience in the field of fashion design',
+    image: 'assets/images/teamm2.png',
+  },
+  {
 
-        name: "Lexandre bravo porte",
-        role: "Designer",
-        bio: 'Designer and project manager, Alexandre is the main project manager of our activities and designer in the women clothing department',
-        image: "assets/images/teamm3.png"
-    },
-    {
+    name: 'Lexandre bravo porte',
+    role: 'Designer',
+    bio: 'Designer and project manager, Alexandre is the main project manager of our activities and designer in the women clothing department',
+    image: 'assets/images/teamm3.png',
+  },
+  {
 
-        name: "Monica Morgan",
-        role: "Technician",
-        bio: 'Franck plays a very important role in our industry,responsible for ensuring that that all machines are working as expected',
-        image: "assets/images/teamm4.png"
-    },
-    {
+    name: 'Monica Morgan',
+    role: 'Technician',
+    bio: 'Franck plays a very important role in our industry,responsible for ensuring that that all machines are working as expected',
+    image: 'assets/images/teamm4.png',
+  },
+  {
 
-        name: "Daniel martin",
-        role: "Supervisor",
-        bio: 'Main supervisor of our activities',
-        image: "assets/images/teamm5.png"
-    },
+    name: 'Daniel martin',
+    role: 'Supervisor',
+    bio: 'Main supervisor of our activities',
+    image: 'assets/images/teamm5.png',
+  },
 ];
 
-
 const teamElement = (name, role, bio, image) => {
-    const el = document.createElement('li');
-    el.className = "team-el";
+  const el = document.createElement('li');
+  el.className = 'team-el';
 
-    el.innerHTML = `
+  el.innerHTML = `
     <div class="trailing">
         <img src="${image}" alt="">
     </div>
@@ -54,18 +53,14 @@ const teamElement = (name, role, bio, image) => {
     </div>
     `;
 
-    return el;
-}
-
-
+  return el;
+};
 
 const printMembers = () => {
-
-    mobileTeamListContainer.forEach((list) => {
-        teamMembers.forEach((el) => {
-            list.appendChild(teamElement(el.name, el.role, el.bio, el.image));
-        });
-    })
-
-}
+  mobileTeamListContainer.forEach((list) => {
+    teamMembers.forEach((el) => {
+      list.appendChild(teamElement(el.name, el.role, el.bio, el.image));
+    });
+  });
+};
 printMembers();
--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,17 +1,14 @@
-const openMenuBtn = document.querySelector(".bx-menu");
-const mobileMenu = document.querySelector(".mobile-menu");
-const closeMenuBtn = document.querySelector(".bx-x");
-
-
+const openMenuBtn = document.querySelector('.bx-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeMenuBtn = document.querySelector('.bx-x');
 
 const openMenu = () => {
-    if (mobileMenu.className.includes("open-mobile-menu")) {
-        mobileMenu.classList.remove("open-mobile-menu");
-    } else {
-        mobileMenu.classList.add("open-mobile-menu");
-    }
-   
-}
+  if (mobileMenu.className.includes('open-mobile-menu')) {
+    mobileMenu.classList.remove('open-mobile-menu');
+  } else {
+    mobileMenu.classList.add('open-mobile-menu');
+  }
+};
 
-openMenuBtn.addEventListener("click", () => openMenu());
-closeMenuBtn.addEventListener("click", () => openMenu());
+openMenuBtn.addEventListener('click', () => openMenu());
+closeMenuBtn.addEventListener('click', () => openMenu());
